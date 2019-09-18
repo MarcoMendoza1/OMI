d5d85b293e278066f20cbb9d26ad3639d5330858
Marco Mendoza <53958595+MarcoMendoza1@users.noreply.github.com>
2019-09-18 14:54:01 -0500

    Create InvasionZombie.cpp

---
+++ InvasionZombie.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n,x,y,c;
+
+long long int bin(){
+	long long int ini=0;
+	long long int fn=200000000;
+	long long int med;
+	
+	while(ini!=fn){
+		med=(ini+fn)/2;
+		if(checa(med)){
+			0
+			
+		}
+		
+	}
+	
+}
+
+int main() {
+	cin>>n>>x>>y>>c;
+	
+	cout<<bin();
+	
+	return 0;
+}
